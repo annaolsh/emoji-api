ae4a8dd452243d32181140cf8f36b9dd5caf41e3
meryldakin <meryl.k.dakin@gmail.com>
2017-05-26 11:55:33 -0400

    set up resources for stories

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/stories', to: 'stories#index'
+  post '/stories', to: 'stories#create'
+  patch '/stories/:id/edit', to: 'stories#update'
+  delete '/stories/:id', to: 'stories#delete'
+  get '/stories/:id', to: 'stories#show'
 end
